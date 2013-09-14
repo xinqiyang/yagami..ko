a4a7585796575a4831e987a4348bcf300e576a0d
xinqiyang <xinqiyang@gmail.com>
2013-09-14 16:40:24 +0900

    update add init_args_simple ,init_args_request method

--- yagami/luasrc/yagami/util.lua
+++ yagami/luasrc/yagami/util.lua
@@ -549,22 +549,26 @@ end
 -- get init args 
 -- thank you for author of method
 -- http://blog.csdn.net/cfeibiao/article/details/8315302
---
+-- get filebody from request then explite filed name and file body
+--[[
+for key,value in pairs(bodydata) do 
+		 local rowfile = util.explode(value,"\r\n\r\n")
+		 local file = io.open(rowfile[1], "w")
+		 file:write(rowfile[2])
+		 file:close()
+end
+--]]	
 function init_args()
 	local args = {}
-	--local file_args = {}
 	local file_body = {}
-	--local is_have_file_param = false
-
 	local receive_headers = ngx.req.get_headers()
 	local request_method = ngx.var.request_method
-	
+	--ngx.log(ngx.DEBUG,"XXXXXXXXXXXXXXXXX method:"..request_method)
 	if "GET" == request_method then
 		args = ngx.req.get_uri_args()
 	elseif "POST" == request_method then
 		ngx.req.read_body()
 		if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then-- if is multipart/form-data form
-			--is_have_file_param = true
 			content_type = receive_headers["content-type"]
 			body_data = ngx.req.get_body_data()--body_data request body not string
 			-- request body size > nginx config is client_body_buffer_size， buffer content to disk，client_body_buffer_size default is 8k or 16k
@@ -589,10 +593,6 @@ function init_args()
 					end
 				end
 			end
-
-
-			--local new_body_data = {}
-			
 			-- get body content
 			if not error_code then
 				local boundary = "--" .. string.sub(receive_headers["content-type"],31)
@@ -608,29 +608,21 @@ function init_args()
 						temp_param_name = string.gsub(temp_param_name,'"','')
 						
 						local temp_param_value = string.sub(t[2],1,-3)
-						--ngx.log(ngx.DEBUG,t[1])
-						--ngx.log(ngx.DEBUG,temp_param_value)
-						
 						args[temp_param_name] = temp_param_value
-					else--file type param，capture paramname，capture2 filename
-						--file_args[capture] = capture2
-						--table.insert(new_body_data,v)
-						--ngx.log(ngx.DEBUG,"```@@@@@")
-						--ngx.log(ngx.DEBUG,v)
-						--table.insert(file_body,capture)
+					else
 						local bd = explode(v,"\r\n\r\n")
 						table.insert(file_body,capture.."\r\n\r\n"..string.sub(bd[2],0,-3))
-						--ngx.log(ngx.DEBUG,"```@@@@@")
 					end
 				end
-				--table.insert(new_body_data,1,first_string)
-				--table.insert(new_body_data,last_string)
-				--body_data = new_body_data
-				--body_data = table.concat(new_body_data,boundary)--body_data  is http request body ,not a common string
 			end
 		else
 			args = ngx.req.get_post_args()
 		end
+	elseif "HEAD" == request_method then
+		ngx.log(ngx.DEBUG,"*****get head method start**********")
+		ngx.log(ngx.DEBUG,table_print(ngx.var))
+		ngx.log(ngx.DEBUG,ngx.var.CONTENT_LENGTH) --content_length
+		ngx.log(ngx.DEBUG,"*****get head method**********")
 	end
 	-- TODO　remove other keys , example : app_key,app_secret
 	-- print request code 
@@ -641,4 +633,164 @@ function init_args()
 	ngx.log(ngx.DEBUG,table_print(file_body))
 	return args,receive_headers,file_body
 end
+
+
+--
+-- get init args 
+-- thank you for author of method
+-- http://blog.csdn.net/cfeibiao/article/details/8315302
+--
+function init_args_simple()
+	local args = {}
+	local receive_headers = ngx.req.get_headers()
+	local request_method = ngx.var.request_method
+	--ngx.log(ngx.DEBUG,"XXXXXXXXXXXXXXXXX method:"..request_method)
+	if "GET" == request_method then
+		args = ngx.req.get_uri_args()
+	elseif "POST" == request_method then
+		ngx.req.read_body()
+		if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then
+			content_type = receive_headers["content-type"]
+			body_data = ngx.req.get_body_data()--body_data request body not string
+			
+			if not body_data then
+				local datafile = ngx.req.get_body_file()
+				if not datafile then
+					error_code = 1
+					error_msg = "no request body found"
+				else
+					local fh, err = io.open(datafile, "r")
+					if not fh then
+						error_code = 2
+						error_msg = "failed to open " .. tostring(datafile) .. "for reading: " .. tostring(err)
+					else
+						fh:seek("set")
+						body_data = fh:read("*a")
+						fh:close()
+						if body_data == "" then
+							error_code = 3
+							error_msg = "request body is empty"
+						end
+					end
+				end
+			end
+			if not error_code then
+				local boundary = "--" .. string.sub(receive_headers["content-type"],31)
+				local body_data_table = explode(tostring(body_data),boundary)
+				local first_string = table.remove(body_data_table,1)
+				local last_string = table.remove(body_data_table)
+				for i,v in ipairs(body_data_table) do
+					local start_pos,end_pos,capture,capture2 = string.find(v,'Content%-Disposition: form%-data; name="(.+)"; filename="(.*)"')
+					if not start_pos then  --common param
+						local t = explode(v,"\r\n\r\n")
+						local temp_param_name = string.match(t[1],'(".-")')
+						temp_param_name = string.gsub(temp_param_name,'"','')
+						local temp_param_value = string.sub(t[2],1,-3)
+						args[temp_param_name] = temp_param_value
+					end
+				end
+			end
+		else
+			args = ngx.req.get_post_args()
+		end
+	elseif "HEAD" == request_method then
+		ngx.log(ngx.DEBUG,"*****get head method start**********")
+		ngx.log(ngx.DEBUG,table_print(ngx.var))
+		ngx.log(ngx.DEBUG,ngx.var.CONTENT_LENGTH) --content_length
+		ngx.log(ngx.DEBUG,"*****get head method**********")
+	end
+
+	local uri         = ngx.var.REQUEST_URI
+    ngx.log(ngx.DEBUG,"\n************** "..request_method.." "..uri.."   ***********")
+	ngx.log(ngx.DEBUG,table_print(args))
+	ngx.log(ngx.DEBUG,table_print(receive_headers))
+	return args,receive_headers
+end
+
+--
+-- get init args 
+-- thank you for author of method
+-- http://blog.csdn.net/cfeibiao/article/details/8315302
+--
+function init_args_request()
+	local args = {}
+	local receive_headers = ngx.req.get_headers()
+	local request_method = ngx.var.request_method
+	
+	if "GET" == request_method then
+		args = ngx.req.get_uri_args()
+	elseif "POST" == request_method then
+		ngx.req.read_body()
+		if string.sub(receive_headers["content-type"],1,20) == "multipart/form-data;" then-- if is multipart/form-data form
+			content_type = receive_headers["content-type"]
+			body_data = ngx.req.get_body_data()--body_data request body not string
+			-- request body size > nginx config is client_body_buffer_size， buffer content to disk，client_body_buffer_size default is 8k or 16k
+			if not body_data then
+				local datafile = ngx.req.get_body_file()
+				if not datafile then
+					error_code = 1
+					error_msg = "no request body found"
+				else
+					local fh, err = io.open(datafile, "r")
+					if not fh then
+						error_code = 2
+						error_msg = "failed to open " .. tostring(datafile) .. "for reading: " .. tostring(err)
+					else
+						fh:seek("set")
+						body_data = fh:read("*a")
+						fh:close()
+						if body_data == "" then
+							error_code = 3
+							error_msg = "request body is empty"
+						end
+					end
+				end
+			end
+			local new_body_data = {}
+			-- get body content
+			if not error_code then
+				local boundary = "--" .. string.sub(receive_headers["content-type"],31)
+				local body_data_table = explode(tostring(body_data),boundary)
+				local first_string = table.remove(body_data_table,1)
+				local last_string = table.remove(body_data_table)
+				for i,v in ipairs(body_data_table) do
+					local start_pos,end_pos,capture,capture2 = string.find(v,'Content%-Disposition: form%-data; name="(.+)"; filename="(.*)"')
+					if not start_pos then  --common param
+						local t = explode(v,"\r\n\r\n")
+						local temp_param_name = string.match(t[1],'(".-")')
+						temp_param_name = string.gsub(temp_param_name,'"','')
+						local temp_param_value = string.sub(t[2],1,-3)
+						args[temp_param_name] = temp_param_value
+					else
+						table.insert(new_body_data,v)
+					end
+				end
+				table.insert(new_body_data,1,first_string)
+				table.insert(new_body_data,last_string)
+				body_data = new_body_data
+				body_data = table.concat(new_body_data,boundary)--body_data  is http request body ,not a common string
+			end
+		else
+			args = ngx.req.get_post_args()
+		end
+	elseif "HEAD" == request_method then
+		ngx.log(ngx.DEBUG,"*****get head method start**********")
+		ngx.log(ngx.DEBUG,table_print(ngx.var))
+		ngx.log(ngx.DEBUG,ngx.var.CONTENT_LENGTH) --content_length
+		ngx.log(ngx.DEBUG,"*****get head method**********")
+	end
+	-- TODO　remove other keys , example : app_key,app_secret
+	-- print request code 
+	local uri         = ngx.var.REQUEST_URI
+    ngx.log(ngx.DEBUG,"\n************** "..request_method.." "..uri.."   ***********")
+	ngx.log(ngx.DEBUG,table_print(args))
+	ngx.log(ngx.DEBUG,table_print(receive_headers))
+	ngx.log(ngx.DEBUG,table_print(body_data))	
+	return args,receive_headers,body_data
+end
+
+
+
+
+
 
